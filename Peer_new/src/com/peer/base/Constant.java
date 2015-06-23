acc8e3f50b6771b4ec851e8ab94e5fe98b421e93
小杨 <395296797@qq.com>
2015-06-23 12:16:55 +0800

    重构新版本

---
+++ Peer_new/src/com/peer/base/Constant.java
@@ -0,0 +1,36 @@
+package com.peer.base;
+
+import java.util.HashSet;
+
+import com.peer.utils.SDCardScanner;
+
+/**
+ * 应用静态变量存储
+ * 
+ * @author zhangzg
+ * 
+ */
+
+public class Constant {
+
+	/**
+	 * SD卡路径
+	 */
+	public static final String SDCARD_DIR = SDCardScanner.getExtSDCardPath();
+	/**
+	 * SharedPreferences名称
+	 */
+	public static final String SHARE_NAME = "peer";
+
+	/**
+	 * 手机屏幕的宽
+	 */
+	public final static String S_SCREEN_WIDHT = "swidth";
+	public static String S_SCREEN_WIDHT_VALUE = "480";
+
+	/**
+	 * 项目存储根目录
+	 */
+	public static final String DEFAULT_MAIN_DIRECTORY = SDCARD_DIR + "/peer/";
+
+}
---
+++ Peer_new/src/com/peer/base/pBaseFragment.java
@@ -0,0 +1,7 @@
+package com.peer.base;
+
+import android.support.v4.app.Fragment;
+
+public class pBaseFragment extends Fragment {
+
+}
