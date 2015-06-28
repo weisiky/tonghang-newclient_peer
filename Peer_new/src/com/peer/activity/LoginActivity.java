98f0e963382d00946eaf81c1a254444ae41a2fb4
zhzhg <nmgchfzhzhg@gmail.com>
2015-06-28 21:50:49 +0800

    1、添加网络请求工具类，见net文件夹下，LoginActivity类

--- Peer_new/src/com/peer/activity/LoginActivity.java
+++ Peer_new/src/com/peer/activity/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.peer.activity;
 
-import org.apache.tools.ant.taskdefs.Sleep;
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.content.Intent;
-import android.content.res.Resources.Theme;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -11,8 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.peer.base.Constant;
 import com.peer.base.pBaseActivity;
+import com.peer.net.HttpUtil;
+import com.peer.net.PeerParamsUtils;
+import com.peer.utils.pLog;
 import com.peer.utils.pViewBox;
 
 /**
@@ -87,7 +95,18 @@ public class LoginActivity extends pBaseActivity {
 		Intent intent = new Intent();
 		switch (v.getId()) {
 		case R.id.bt_login_login:
-			startActivityForLeft(MainActivity.class, intent, false);
+			String email = pageViewaList.et_email_login.getText().toString()
+					.trim();
+			String password = pageViewaList.et_password_login.getText()
+					.toString().trim();
+
+			if (email.length() > 0 && password.length() > 0) {
+				showProgressBar();
+				sendLoginRequest(email, password);
+			} else {
+				showToast("请输入用户名与密码", Toast.LENGTH_SHORT, false);
+			}
+
 			break;
 		case R.id.tv_register_login:
 			startActivityForLeft(RegisterAcountActivity.class, intent, false);
@@ -96,6 +115,93 @@ public class LoginActivity extends pBaseActivity {
 			break;
 		}
 
+	}
+
+	/**
+	 * 请求登录接口
+	 * 
+	 * @param email
+	 * @param password
+	 */
+
+	private void sendLoginRequest(String email, String password) {
+		// TODO Auto-generated method stub
+		final Intent intent = new Intent();
+		RequestParams params = PeerParamsUtils.getLoginParams(
+				LoginActivity.this, email, password);
+		HttpUtil.get(Constant.LONIN_IN_URL, params,
+				new JsonHttpResponseHandler() {
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							String responseString, Throwable throwable) {
+						// TODO Auto-generated method stub
+
+						hideLoading();
+						pLog.i("test", "onFailure+statusCode:" + statusCode + "headers:"
+								+ headers.toString() + "responseString:"
+								+ responseString);
+
+						super.onFailure(statusCode, headers, responseString,
+								throwable);
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							Throwable throwable, JSONArray errorResponse) {
+						// TODO Auto-generated method stub
+						hideLoading();
+						pLog.i("test", "onFailure+statusCode:" + statusCode + "headers:"
+								+ headers.toString() + "errorResponse:"
+								+ errorResponse.toString());
+						super.onFailure(statusCode, headers, throwable,
+								errorResponse);
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							Throwable throwable, JSONObject errorResponse) {
+						// TODO Auto-generated method stub
+						hideLoading();
+						pLog.i("test", "onFailure:statusCode:" + statusCode);
+						super.onFailure(statusCode, headers, throwable,
+								errorResponse);
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							JSONArray response) {
+						// TODO Auto-generated method stub
+						hideLoading();
+						pLog.i("test", "onSuccess+statusCode:" + statusCode + "headers:"
+								+ headers.toString() + "response:"
+								+ response.toString());
+						super.onSuccess(statusCode, headers, response);
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							JSONObject response) {
+						// TODO Auto-generated method stub
+						hideLoading();
+						pLog.i("test", "onSuccess:statusCode:" + statusCode + "headers:"
+								+ headers.toString() + "response:"
+								+ response.toString());
+						super.onSuccess(statusCode, headers, response);
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							String responseString) {
+						// TODO Auto-generated method stub
+						hideLoading();
+						pLog.i("test", "onSuccess:statusCode:" + statusCode + "headers:"
+								+ headers.toString() + "responseString:"
+								+ responseString.toString());
+						super.onSuccess(statusCode, headers, responseString);
+					}
+
+				});
 	}
 
 	@Override
--- Peer_new/src/com/peer/base/Constant.java
+++ Peer_new/src/com/peer/base/Constant.java
@@ -32,8 +32,14 @@ public class Constant {
 	 * 项目存储根目录
 	 */
 	public static final String DEFAULT_MAIN_DIRECTORY = SDCARD_DIR + "/peer/";
-	
+
 	/** 文件在本地的缓存目录 **/
-	public static String C_FILE_CACHE_PATH = DEFAULT_MAIN_DIRECTORY+"filecache/";
+	public static String C_FILE_CACHE_PATH = DEFAULT_MAIN_DIRECTORY
+			+ "filecache/";
+	/** 服务器地址 **/
+	public static String SERVER_ADDRESS = "www.baidu.com";
+//	public static String SERVER_ADDRESS = "http://127.0.0.1:8080/tonghang-serverapi";
+	/** 登录请求地址 **/
+	public static String LONIN_IN_URL = SERVER_ADDRESS + "/user/login.json";
 
 }
